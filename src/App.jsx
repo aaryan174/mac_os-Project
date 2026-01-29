dfcd5de21f62f2d9add17b988c5c911195f08af1
Aryan Patel <aryan007.lko@gmail.com>
2026-01-29 22:31:58 +0530

    making of github page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import "./App.scss"
 import Dock from './components/Dock'
 import Nav from './components/Nav'
+import MacWindow from './components/Windows/MacWindow'
+import Github from './components/Windows/Github'
 
 const App = () => {
   return (
      <main>
       <Nav />
       <Dock />
+      <Github />
      </main>
   )
 }
--- src/components/Windows/MacWindow.jsx
+++ src/components/Windows/MacWindow.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import { Rnd } from 'react-rnd'
-import "./Window.scss"
+import "./window.scss"
 
-const MacWindow = ({children}) => {
-  return (
-    <Rnd>
-        <div className="window">
-            <div className="nav">
-                <div className="dots">
-                    <div className="dot red"></div>
-                    <div className="dot yellow"></div>
-                    <div className="dot green"></div>
+const MacWindow = ({ children, width = "40vw", height = "40vh", windowName, setWindowsState }) => {
+    return (
+        <Rnd
+            default={{
+                width: width,
+                height: height,
+                x: 300,
+                y: 200
+            }}
+        >
+            <div className="window">
+                <div className="nav">
+                    <div className="dots">
+                        <div onClick={() => setWindowsState(state => ({ ...state, [windowName]: false }))} className="dot red"></div>
+                        <div className="dot yellow"></div>
+                        <div className="dot green"></div>
+                    </div>
+
+                    <div className="title"><p>aryanpatel - zsh</p></div>
+
+                </div>
+                <div className="main-content">
+                    {children}
                 </div>
             </div>
-            <div className="main-contant">{children}</div>
-        </div>
-        
-    </Rnd>
-  )
+        </Rnd>
+    )
 }
 
 export default MacWindow
